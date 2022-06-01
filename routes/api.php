cf1951f607234f922e0431dc94dd2e951c9431c7
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-01 16:46:44 -0300

    Adiciona auth no começo do endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\UserController;
 
-Route::post('signIn', [AuthController::class, 'login']);
+Route::post('auth/signIn', [AuthController::class, 'login']);
 
 Route::middleware(['apiJwt'])->group(function () {
-    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('auth/logout', [AuthController::class, 'logout']);
 
     Route::apiResource('users', UserController::class);
 });
