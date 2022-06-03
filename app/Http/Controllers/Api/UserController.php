31d7186a2858a16f142582d563aac9d8d1247a0f
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-03 20:10:58 -0300

    Centraliza lógica nos Services

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,76 +5,15 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Services\UserService;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
 
-    public function __construct(UserService $service)
+    public function __construct(User $model, UserService $service)
     {
+        $this->model = $model;
         $this->service = $service;
     }
 
-    public function index() {
-        $users = User::all();
-        return response()->json($users);
-    }
 
-    /**
-        * @OA\Post(
-        * path="/api/users",
-        * tags={"Store"},
-        * summary="User Store",
-        *     @OA\RequestBody(
-        *         @OA\JsonContent(),
-        *         @OA\MediaType(
-        *            mediaType="json",
-        *            @OA\Schema(
-        *               type="object",
-        *               required={"name","email", "password", "password_confirmation"},
-        *               @OA\Property(property="name", type="string"),
-        *               @OA\Property(property="email", type="string"),
-        *               @OA\Property(property="password", type="string"),
-        *               @OA\Property(property="password_confirmation", type="string")
-        *            ),
-        *        ),
-        *    ),
-        *      @OA\Response(
-        *          response=201,
-        *          description="Register Successfully",
-        *          @OA\JsonContent()
-        *       ),
-        *      @OA\Response(
-        *          response=200,
-        *          description="Register Successfully",
-        *          @OA\JsonContent()
-        *       ),
-        *      @OA\Response(
-        *          response=422,
-        *          description="Unprocessable Entity",
-        *          @OA\JsonContent()
-        *       ),
-        *      @OA\Response(response=400, description="Bad request"),
-        *      @OA\Response(response=404, description="Resource Not Found"),
-        * )
-        */
-    public function store(Request $request) {
-        return $this->service->store($request);
-    }
-
-    public function show($id)
-    {
-        //
-    }
-
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    public function destroy($id)
-    {
-        //
-    }
 }
