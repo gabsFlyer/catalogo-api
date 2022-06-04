d5509b7814357d705e35faa0a0834a34f351f849
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-03 22:25:27 -0300

    Adiciona unidade de medida

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,24 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+    protected $with = ['measurementUnit'];
+
     protected $fillable = [
         'name',
         'description',
         'weight',
-        //'measurement_unit',
         'purchase_price',
         'unit_price',
         'wholesale_price',
         'wholesale_minimum_quantity',
-        'maximum_percent_discount'
+        'maximum_percent_discount',
+        'measurement_unit_id'
     ];
 
-    public function rules(){
+    protected $hidden = [
+        'measurement_unit_id'
+    ];
+
+    public function rules() {
         return [
             'name' => 'required',
             'unit_price' => 'required|gt:0',
             'wholesale_price' => 'required|gt:0',
             'wholesale_minimum_quantity' => 'required|gt:0'
         ];
+    }
+
+    public function measurementUnit() {
+        return $this->belongsTo(MeasurementUnit::class);
     }
 }
