d4dd58b4f69dc1b191aa4ecfa2c32552c2ee4228
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-03 21:10:59 -0300

    Cria cadastro de unidades de medida

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\FileController;
+use App\Http\Controllers\Api\MeasurementUnitController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
 
@@ -13,8 +14,9 @@ Route::middleware(['apiJwt'])->group(function () {
     Route::post('auth/me', [AuthController::class, 'me']);
     Route::post('auth/logout', [AuthController::class, 'logout']);
 
+    Route::apiResource('measurement-unit', MeasurementUnitController::class);
     Route::apiResource('file', FileController::class);
-    Route::apiResource('user', UserController::class);
     Route::apiResource('product', ProductController::class);
+    Route::apiResource('user', UserController::class);
 });
 
