6eee9a9e213122966228a6a24b34131ec293c2f3
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-06 21:10:28 -0300

    Recebe o id do measurement unit pelo objeto

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,11 +2,24 @@
 namespace App\Services;
 
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductService extends Service
 {
     public function __construct(Product $model)
     {
         $this->model = $model;
+    }
+
+    public function store(Request $request) {
+        $measurementUnit = $request->input('measurement_unit');
+
+        if($measurementUnit != null && in_array('id', array_keys($measurementUnit))){
+            $request->merge([
+                'measurement_unit_id' => $measurementUnit['id']
+            ]);
+        }
+
+        return parent::store($request);
     }
 }
