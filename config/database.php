fdd9354e6790b2a8aeb2bba452f0b913b1a09146
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-18 22:36:38 -0300

    Adiciona local do dump no .env

--- config/database.php
+++ config/database.php
@@ -62,7 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
             'dump' => [
-                'dump_binary_path' => 'C:/xampp/mysql/bin/',
+                'dump_binary_path' => env('MYSQL_DUMP_PATH'),
                 'use_single_transaction',
                 'timeout' => 60 * 5,
             ],
