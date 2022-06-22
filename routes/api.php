b6ce4b3c4c3fa54eccac4db121aebf15551396dd
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-21 23:45:13 -0300

    Cria rota /optimize

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\FileController;
 use App\Http\Controllers\Api\MeasurementUnitController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\UserController;
+use Illuminate\Support\Facades\Artisan;
 
 Route::post('auth/signIn', [AuthController::class, 'signIn']);
 Route::post('auth/signUp', [AuthController::class, 'signUp']);
@@ -20,3 +21,7 @@ Route::middleware(['apiJwt'])->group(function () {
     Route::apiResource('user', UserController::class);
 });
 
+Route::get('/optimize', function() {
+    $artisanExit = Artisan::call('config:cache');
+    return response()->json(['message' => $artisanExit], 200);
+});
