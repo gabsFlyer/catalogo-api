c50f22cb346b53940da9eae622d09d21a8105bbf
Gabriel Monteiro <gmonteeeiro@gmail.com>
2022-06-25 22:57:06 -0300

    Adiciona lista de relacionamentos ao service genérico

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -2,37 +2,13 @@
 namespace App\Services;
 
 use App\Models\Product;
-use Illuminate\Http\Request;
 
 class ProductService extends Service
 {
+    protected $relationships = ['measurement_unit', 'file'];
+
     public function __construct(Product $model)
     {
         $this->model = $model;
-    }
-
-    public function store(Request $request) {
-        $request = $this->moveIdFromObject($request, 'measurement_unit');
-        $request = $this->moveIdFromObject($request, 'file');
-
-        return parent::store($request);
-    }
-
-    public function update(Request $request, $id) {
-        $request = $this->moveIdFromObject($request, 'measurement_unit');
-        $request = $this->moveIdFromObject($request, 'file');
-
-        return parent::update($request, $id);
-    }
-
-    private function moveIdFromObject(Request $request, $table) {
-        $child = $request->input($table);
-        if($child != null && in_array('id', array_keys($child))){
-            $request->merge([
-                "{$table}_id" => $child['id']
-            ]);
-        }
-
-        return $request;
     }
 }
